74b43b471620d31d7e274aab79f3a66d7ddd708e
nidakul <nidakul@gmail.com>
2023-09-08 22:16:50 +0300

    Day 7 - assignment 2

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CarManager carManager = new CarManager(new InMemoryCarDal());
+
+        foreach (var car in carManager.GetById(1))
+        {
+            Console.WriteLine(car.BrandId);
+        }
+
+        
+    }
+}
+
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System.Linq;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> {
+                new Car { CarId = 1, BrandId = 2, ColorId = 3, ModelYear = 1995, DailyPrice = 135.34m, Description = "New Car" },
+                new Car { CarId = 2, BrandId = 4, ColorId = 4, ModelYear = 1985, DailyPrice = 136.34m, Description = "New Car2" },
+                new Car { CarId = 3, BrandId = 1, ColorId = 2, ModelYear = 2004, DailyPrice = 134.34m, Description = "New Car3" },
+                new Car { CarId = 4, BrandId = 2, ColorId = 1, ModelYear = 2007, DailyPrice = 123.34m, Description = "New Car4" },
+                new Car { CarId = 5, BrandId = 3, ColorId = 3, ModelYear = 2018, DailyPrice = 193.34m, Description = "New Car5" }
+        };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(p => p.CarId == car.CarId);
+
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void GetById(Car carId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(c => c.CarId == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+
+        }
+    }
+}
+
